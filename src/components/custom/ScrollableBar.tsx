086408173ba1fde9f743021cf8a6eb2e165059d8
KimDanwoo <84695769+KimDanwoo@users.noreply.github.com>
2023-05-31 16:21:17 +0900

    :art: following bar 완성

---
+++ src/components/custom/ScrollableBar.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+import Carousel from 'react-multi-carousel'
+import 'react-multi-carousel/lib/styles.css'
+
+const responsive = {
+  desk: {
+    breakpoint: { max: 4000, min: 576 },
+    items: 6,
+    slidesToSlide: 4, // optional, default to 1.
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 6,
+    slidesToSlide: 2, // optional, default to 1.
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 4,
+    slidesToSlide: 1, // optional, default to 1.
+  },
+}
+
+export default function ScrollableBar({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <Carousel responsive={responsive} containerClass="flex w-full mx-auto p-4">
+      {children}
+    </Carousel>
+  )
+}
--- src/components/main/FollowingBar.tsx
+++ src/components/main/FollowingBar.tsx
@@ -6,13 +6,13 @@ import { SyncLoader } from 'react-spinners'
 import useSWR from 'swr'
 import Avatar from './Avatar'
 import { useSession } from 'next-auth/react'
+import ScrollableBar from '../custom/ScrollableBar'
 
 export default function FollowingBar() {
   const { data: session } = useSession()
   const user = session?.user
   const { data, isLoading: loading, error } = useSWR<DetailUser>('/api/me')
   const users = data?.following
-  console.log(data?.following)
   return (
     <section>
       {loading ? (
@@ -31,28 +31,25 @@ export default function FollowingBar() {
         )
       )}
       {users && users?.length > 0 && (
-        <ul className="flex w-[95%] mx-auto p-4">
-          <li className="block xl:hidden">
-            <Link
-              href={`/user/${user?.username}`}
-              className="flex flex-col items-center mx-2"
-            >
-              <Avatar image={user?.image} size={16}></Avatar>
-              <p className=" text-xs mt-2">{user?.username}</p>
-            </Link>
-          </li>
+        <ScrollableBar>
+          <Link
+            href={`/user/${user?.username}`}
+            className="flex flex-col items-center mx-2"
+          >
+            <Avatar image={user?.image} size={16}></Avatar>
+            <p className=" text-xs mt-2">{user?.username}</p>
+          </Link>
           {users.map(({ username, image }) => (
-            <li key={username}>
-              <Link
-                href={`/user/${username}`}
-                className="flex flex-col items-center mx-2"
-              >
-                <Avatar image={image} size={16}></Avatar>
-                <p className=" text-xs mt-2">{username}</p>
-              </Link>
-            </li>
+            <Link
+              href={`/user/${username}`}
+              className="flex flex-col items-center mx-2"
+              key={username}
+            >
+              <Avatar image={image} size={16}></Avatar>
+              <p className=" text-xs mt-2">{username}</p>
+            </Link>
           ))}
-        </ul>
+        </ScrollableBar>
       )}
     </section>
   )
