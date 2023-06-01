c76bbd40de297155ecb39d56bc2ed83339ba3da5
KimDanwoo <84695769+KimDanwoo@users.noreply.github.com>
2023-06-02 05:55:38 +0900

    :art: 모달 상세 구현

--- src/components/posts/PostListCard.tsx
+++ src/components/posts/PostListCard.tsx
@@ -47,7 +47,47 @@ export default function PostListCard({ post, priority = false }: Props) {
       {openModal && (
         <ModalPortal>
           <PostModal onClose={() => setOpenModal(false)}>
-            <div>하이루</div>
+            <div className="w-[70%] md:w-[90%]  h-[90%] max-w-[1100px] max-h-[631px] bg-white rounded-md md:flex">
+              <div className="flex py-2 items-center p-4 md:hidden">
+                <Avatar image={userImage} size={7} />
+                <span className="ml-4 text-sm font-bold">{username}</span>
+                <span className="ml-4 text-sm text-gray-400">
+                  · {parseDate(createdAtStr)}
+                </span>
+              </div>
+              <div className="w-full h-[265px] md:h-[100%] max-w-[631px] max-h-[631px] object-cover">
+                <Image
+                  className="w-full h-full object-cover md:rounded-l-md"
+                  src={image}
+                  width={465}
+                  height={465}
+                  alt={`photo by ${id}`}
+                  priority={priority}
+                />
+              </div>
+
+              <div className="w-[100%] h-[50%] md:h-[100%] flex flex-col justify-between ">
+                <div className="hidden py-2 items-center p-4  md:flex ">
+                  <Avatar image={userImage} size={7} />
+                  <span className="ml-4 text-sm font-bold">{username}</span>
+                  <span className="ml-4 text-sm text-gray-400">
+                    · {parseDate(createdAtStr)}
+                  </span>
+                </div>
+                <div>
+                  
+                </div>
+                <div className="p-4">
+                  <ActionBar
+                    likes={likes}
+                    username={username}
+                    text={text}
+                    openModal={() => setOpenModal(true)}
+                  />
+                  <CommentForm />
+                </div>
+              </div>
+            </div>
           </PostModal>
         </ModalPortal>
       )}
